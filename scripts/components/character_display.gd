41ce7e3fb7528b133f77abcdb323890ab520e67d
ZedManul <zedthemanul@gmail.com>
2025-09-28 23:28:12 +0300

    I def changed -something-

--- scripts/components/character_display.gd
+++ scripts/components/character_display.gd
@@ -5,7 +5,7 @@ extends Node2D
 @export var ik_target_root: Node2D
 @export var run_to_idle_rate: float = 2.0
 @export var idle_to_run_rate: float = 8.0
-@export var land_to_idle_rate: float = 2.0
+@export var land_to_idle_rate: float = 3.0
 
 
 func _process(delta: float) -> void:
@@ -21,7 +21,7 @@ func _process(delta: float) -> void:
 	anim_tree["parameters/blend_landing/blend_amount"] = move_toward(
 			anim_tree["parameters/blend_landing/blend_amount"],
 			0.0,
-			land_to_idle_rate * delta
+			land_to_idle_rate * (1.0 + abs(velocity_ratio_x))/2 * delta
 		)
 	
 	if player_node.is_on_floor():
